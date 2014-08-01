8c7b6564453b100ecd30097a1f82b54813d14108
Gene Leynes <gleynes@gmail.com>
2014-08-01 14:31:46 -0500

    fixed round_weeks

--- R/round_weeks.R
+++ R/round_weeks.R
@@ -1,16 +1,23 @@
 round_weeks <- function(x){
 	require(data.table)
-	dt <- data.table(i = 1:length(x),
+	dt <- data.table::data.table(i = 1:length(x),
 					 day = x,
 					 weekday = weekdays(x))
-	offset <- data.table(weekday = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 
+	offset <- data.table::data.table(weekday = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 
 									 'Thursday', 'Friday', 'Saturday'),
-						 offset = -(0:6))
+						 offset_amt = -(0:6))
 	dt <- merge(dt, offset, by="weekday")
-	
-    # dt[ , day_adj := day + offset]
-	dt$day_adj <- dt$day + dt$offset
-	
+    dt[ , day_adj := day + offset_amt]
     setkey(dt, i)
 	return(dt$day_adj)
 }
+
+
+if(FALSE){
+    ## a test:
+    require(data.table)
+    dd <- seq(as.IDate("2013-12-20"), as.IDate("2014-01-20"), 1)
+    round_weeks(dd)
+}
+    
+    
