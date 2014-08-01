279713968cc0bf2b9e23d6a0e9842109cec75817
Gene Leynes <gleynes@gmail.com>
2014-08-01 14:39:52 -0500

    minor changes

--- R/round_weeks.R
+++ R/round_weeks.R
@@ -1,9 +1,9 @@
 round_weeks <- function(x){
 	require(data.table)
-	dt <- data.table::data.table(i = 1:length(x),
+	dt <- data.table(i = 1:length(x),
 					 day = x,
 					 weekday = weekdays(x))
-	offset <- data.table::data.table(weekday = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 
+	offset <- data.table(weekday = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 
 									 'Thursday', 'Friday', 'Saturday'),
 						 offset_amt = -(0:6))
 	dt <- merge(dt, offset, by="weekday")
@@ -15,8 +15,10 @@ round_weeks <- function(x){
 
 if(FALSE){
     ## a test:
+    library(geneorama)
     require(data.table)
     dd <- seq(as.IDate("2013-12-20"), as.IDate("2014-01-20"), 1)
+    round(dd, 'weeks')
     round_weeks(dd)
 }
     
