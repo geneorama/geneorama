bfd052a54d71443a58e17f580af3e78c3f880c2f
Gene Leynes <gleynes@gmail.com>
2015-12-23 14:26:28 -0600

    fix for nested lists

--- R/json_to_datatable.R
+++ R/json_to_datatable.R
@@ -1,20 +1,21 @@
 json_to_datatable <- function(lst){
+    # browser()
     require(jsonlite)
     ret <- list()
     for(i in 1:length(lst)){
         if(i %% 1000 == 1) print(i)
-        cur <- fromJSON(lst[[i]])
+        cur <- unlist(fromJSON(lst[[i]]))
         for(newname in names(cur)[!names(cur) %in% names(ret)]){
-            ret[[newname]] <- vector(mode = class(cur[[newname]]), 
+            ret[[newname]] <- vector(mode = "character", 
                                      length = length(lst))
         }
         for(curname in names(cur)){
-            if(length(cur[[curname]])!=0){
+            if(!is.null(cur[[curname]])){
                 ret[[curname]][i] <- cur[[curname]]
             }
         }
     }
+    ret <- lapply(ret, type.convert, na.strings = c("", "NA"), as.is = TRUE)
     ret <- as.data.table(ret)
     return(ret)
-    
 }
