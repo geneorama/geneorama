16504ef0ff78082cf7dab80e5b394d4010465546
Gene Leynes <gleynes@gmail.com>
2015-12-23 11:47:59 -0600

    adding json_to_data.table function

---
+++ R/json_to_datatable.R
@@ -0,0 +1,18 @@
+json_to_datatable <- function(lst){
+    require(jsonlite)
+    ret <- list()
+    for(i in 1:length(lst)){
+        if(i %% 1000 == 1) print(i)
+        cur <- fromJSON(lst[[i]])
+        for(newname in names(cur)[!names(cur) %in% names(ret)]){
+            ret[[newname]] <- vector(mode = class(cur[[newname]]), 
+                                     length = length(lst))
+        }
+        for(curname in names(cur)){
+            ret[[curname]][i] <- cur[[curname]]
+        }
+    }
+    ret <- as.data.table(ret)
+    return(ret)
+    
+}
