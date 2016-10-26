b17b44343ff1374c01334a297c183b9af7b05383
Gene Leynes <gleynes@gmail.com>
2016-10-26 13:34:42 -0500

    ading function to convert line endings

---
+++ R/convert_crlf.R
@@ -0,0 +1,27 @@
+#' @name   convert_crlf
+#' @title  convert crlf to lf in Windows
+#' @author Gene Leynes
+#' 
+#' @param infile File to be converted
+#' 
+#' @description
+#' 		Rewrite a file and substitute LF for CRLF.
+#'
+#' @details
+#' 		LF is Unix.  Note that writeChar, writeLines, and other options don't 
+#' 		work (as far as I can tell). Using cat on a wb file connection is the 
+#' 		key.
+#'
+#' @seealso
+#' 	\code{\link{cat}}, \code{\link{readLines}}, \code{\link{file}}
+#'
+#'
+
+
+convert_crlf <- function(infile){
+	print(infile)
+	txt <- readLines(infile)
+	f <- file(infile, open="wb")
+	cat(txt, file=f, sep="\n")
+	close(f)
+}
