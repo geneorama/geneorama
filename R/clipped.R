3118d167b896518ff3b6496c804a97886d7d0872
gene <gene.leynes@cityofchicago.org>
2020-06-03 12:55:06 -0500

    new clipboard functions

--- R/clipped.R
+++ R/clipped.R
@@ -29,19 +29,80 @@
 #'		str(clipped_example)
 #'		}
 #'
+#' New version based on:
+#' https://stackoverflow.com/questions/10959521/how-to-write-to-clipboard-on-ubuntu-linux-in-r
+#' 
 
-
-clipped <- function(lowercase=TRUE, removedot=TRUE){
-	x <- read.delim("clipboard", stringsAsFactors=FALSE)
-	if (lowercase) colnames(x) <- tolower(colnames(x))
-	if (removedot) colnames(x) <- gsub('\\.','',colnames(x))
-	return(x)
+clipped = function(header = TRUE,
+				   sep = "\t",
+				   na.strings = c("", "NA"),
+				   check.names = FALSE,
+				   stringsAsFactors = F,
+				   dec = ".",
+				   as.data.table = TRUE,
+				   remove.spaces = TRUE,
+				   ...) {
+	#decide how to read
+	#windows is easy!
+	if (Sys.info()['sysname'] %in% c("Windows")) {
+		#just read as normal
+		ret <- read.table(file = con,
+						  sep = sep, 
+						  header = header,
+						  check.names = check.names,
+						  na.strings = na.strings,
+						  stringsAsFactors = stringsAsFactors,
+						  dec = dec, 
+						  ...)
+	} else {
+		## Unix: try xclip approach 
+		## https://stackoverflow.com/a/10960498/3980197
+		
+		## Define xclip function
+		read.xclip = function(x) {
+			#if xclip not installed
+			if (!isTRUE(file.exists(Sys.which("xclip")[1L]))) {
+				stop("Cannot find xclip")
+			}
+			con <- pipe("xclip -o -selection c", "r")
+			on.exit(close(con))
+			ret1 <- read.table(file = con,
+							   sep = sep,
+							   header = header,
+							   check.names = check.names,
+							   na.strings = na.strings,
+							   stringsAsFactors = stringsAsFactors,
+							   dec = dec,
+							   ...)
+			
+			return(ret1)
+		}
+		
+		## Try xclip function
+		tryCatch({
+			ret <- read.xclip(x)
+		}, error = function(e) {
+			message(sprintf("error: %s", e$message))
+		})
+	}
+	
+	## Convert return to data.table
+	if(as.data.table){
+		ret <- data.table::as.data.table(ret)				
+	}
+	
+	## Remove spaces from column names
+	if(remove.spaces){
+		data.table::setnames(ret, gsub(" ", "_", colnames(ret)))
+	}
+	
+	## Return result
+	return(ret)
 }
 
 
-
-
-
-
-
-
+if(FALSE){
+	source("R/clipped.R")
+	examp <- clipped()
+	examp
+}
--- R/clipper.R
+++ R/clipper.R
@@ -26,13 +26,65 @@
 #'		clipper(matrix(rnorm(100),10,10))
 #'		}
 #'
+#' New version based on:
+#' https://stackoverflow.com/questions/10959521/how-to-write-to-clipboard-on-ubuntu-linux-in-r
+#' 
 
 
-clipper <- function(x){
-	write.table(x,"clipboard", sep="\t", col.names=TRUE, row.names=FALSE)
+clipper = function(x, 
+				   quote = TRUE, 
+				   sep = "\t", 
+				   eol = "\n", 
+				   na = "", 
+				   dec = ".", 
+				   row.names = FALSE, 
+				   col.names = TRUE, 
+				   qmethod = c("escape", "double")) {
+	#decide how to write
+	#windows is easy!
+	if (Sys.info()['sysname'] %in% c("Windows")) {
+		#just write as normal
+		write.table(x = x,
+					file = "clipboard", 
+					sep = sep, 
+					eol = eol,
+					na = na,
+					dec = dec,
+					row.names = row.names,
+					col.names = col.names,
+					qmethod = qmethod)
+	} else {
+		#for non-windows, try xclip approach
+		#https://stackoverflow.com/a/10960498/3980197
+		write.xclip = function(x) {
+			#if xclip not installed
+			if (!isTRUE(file.exists(Sys.which("xclip")[1L]))) {
+				stop("Cannot find xclip")
+			}
+			con <- pipe("xclip -selection c", "w")
+			on.exit(close(con))
+			write.table(x, 
+						con, 
+						sep = sep, 
+						eol = eol,
+						na = na,
+						dec = dec,
+						row.names = row.names,
+						col.names = col.names,
+						qmethod = qmethod)
+		}
+		
+		tryCatch({
+			write.xclip(x)
+		}, error = function(e) {
+			message("Could not write using xclip")
+		})
+	}
 }
 
-
+if(FALSE){
+	clipper(data.frame(x=1:4, y=letters[1:4]))
+}
 
 
 
