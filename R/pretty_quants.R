f1c4f91a8beba9b250a035068dfcefdd8e1f7533
gene <gene.leynes@cityofchicago.org>
2020-06-25 16:20:11 -0500

    fix

--- R/pretty_quants.R
+++ R/pretty_quants.R
@@ -1,6 +1,5 @@
 
 pretty_quants <- function(vec, n=10){
-    vec <- shpTracts@data$total
     quants <- quantile(vec, na.rm = T, probs = seq(0,1,1/n))
     prettyquants <- sapply(quants, pretty)
     breaks <- unname(c(prettyquants[1,], max(prettyquants)))
