22640d3beade12a5b61582a69fb6d37bb137039f
gene <gene.leynes@cityofchicago.org>
2020-10-15 17:09:27 -0500

    adding ma moving average fun

---
+++ R/ma.R
@@ -0,0 +1,57 @@
+#' @name   ma
+#' @title  Calculate a rolling average
+#' @author Gene Leynes
+#' 
+#' @param x            Vector to average.
+#' @param p            Number of periods to go back.
+#' @param sides        Use 1 for trailing average, 2 for center, see Details
+#'
+#' @description
+#' 		Calculates a moving average for p periods.  
+#'
+#' @details
+#'   	The function uses filter, which returns a time series, but that is 
+#'   	converted back to a vector.
+#'   	
+#'   	The sides are passed to the filter function, see that documentation for
+#'   	details. The two more useful modes are 1 for a forward looking moving
+#'   	average, and 2 for centered. 
+#'
+#' @seealso
+#' 	\code{\link{filter}},
+#' 	\code{\link{convolve}}
+#'
+#' @examples
+#'
+#'      require(geneorama)
+#'
+#'      ma(1:20)
+#'      ma(c(1:20, rep(NA,3), 21:30))
+#'
+
+
+ma <- function(x, 
+			   p = 5,
+			   sides = 1){
+	ret <- stats::filter(x,
+						 rep(1 / p, p), 
+						 sides = sides)
+	ret <- unclass(ret)
+	attr(ret, "tsp") <- NULL
+	return(ret)
+}
+
+if(FALSE){
+	ma(c(1:20, rep(NA,1), 21:30))
+	ma(c(1:20, rep(NA,3), 21:30))
+	
+	
+	Han <- function(y) # Hanning{
+		convolve(y, c(1,2,1)/4, type = "filter")
+	}
+	plot(x, y, main = "Using  convolve(.) for Hanning filters")
+	lines(x[-c(1  , n)      ], Han(y), col = "red")
+	lines(x[-c(1:2, (n-1):n)], Han(Han(y)), lwd = 2, col = "dark blue")
+	
+}
+
